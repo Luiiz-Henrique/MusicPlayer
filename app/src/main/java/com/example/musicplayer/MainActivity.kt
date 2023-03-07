9c3184ad3d42ccb74a0ee0350bc35858841e5f69
Luiiz-Henrique <luizhenriquesb21@gmail.com>
2023-03-07 16:07:18 -0300

    Final (adicionado a parte lógica) - 07/03/2023

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.musicplayer
 
+import android.annotation.SuppressLint
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import android.util.Log
+import android.view.MenuItem
 import android.widget.ImageButton
 import android.widget.SeekBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
@@ -18,6 +24,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var remaining: TextView
     lateinit var position: SeekBar
     lateinit var volume: SeekBar
+    private var totalTime = 0
 
     private lateinit var drawerTogle: DrawerLayout
     private lateinit var navView: NavigationView
@@ -34,7 +41,7 @@ class MainActivity : AppCompatActivity() {
         position = findViewById(R.id.position)
         volume = findViewById(R.id.volume)
 
-        mp = MediaPlayer.create(this, R.raw.Believer)
+        mp = MediaPlayer.create(this, R.raw.believer)
 
         drawerTogle = findViewById(R.id.drawerlayout)
         toggle = ActionBarDrawerToggle(this, drawerTogle, R.string.open, R.string.close)
@@ -42,5 +49,72 @@ class MainActivity : AppCompatActivity() {
 
 
 
+        play.setOnClickListener {
+            if (mp.isPlaying) {
+                mp.pause()
+                play.setImageResource(R.drawable.play)
+            } else {
+                mp.start()
+                play.setImageResource(R.drawable.pause)
+            }
+        }
+        mp.isLooping = true
+        mp.setVolume(0.5f, 0.5f)
+        totalTime = mp.duration
+        position.max = totalTime
+
+
+        Music.setSeekBar(volume, mp, true)
+        Music.setSeekBar(position, mp, null, true)
+
+
+        val handler = @SuppressLint("HandlerLeak")
+        object : Handler() {
+            override fun handleMessage(msg: Message) {
+                val currentPosition = msg.what
+                position.progress = currentPosition
+                val elapsedTime = Music.createTimeLabel(currentPosition)
+                elapsed.text = elapsedTime
+                val remainingTime = Music.createTimeLabel(totalTime - currentPosition)
+                remaining.text = "- $remainingTime"
+            }
+        }
+        Thread(Runnable {
+            while (true) {
+                try {
+                    val msg = Message()
+                    msg.what = mp.currentPosition
+                    handler.sendMessage(msg)
+                    Thread.sleep(1000)
+                } catch (e: InterruptedException) {
+                    Log.d("Thread", e.message.toString())
+                }
+            }
+        }).start()
+
+
+        drawerTogle.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> Toast.makeText(this, "Clicked Home", Toast.LENGTH_SHORT).show()
+                R.id.filemusic -> Toast.makeText(this, "Clicked File", Toast.LENGTH_SHORT).show()
+                R.id.sync -> Toast.makeText(this, "Clicked Sync", Toast.LENGTH_SHORT).show()
+                R.id.trash -> Toast.makeText(this, "Clicked Trash", Toast.LENGTH_SHORT).show()
+                R.id.settings -> Toast.makeText(this, "Clicked Settings", Toast.LENGTH_SHORT).show()
+                R.id.login -> Toast.makeText(this, "Clicked Login", Toast.LENGTH_SHORT).show()
+                R.id.share -> Toast.makeText(this, "Clicked Share", Toast.LENGTH_SHORT).show()
+                R.id.rateus -> Toast.makeText(this, "Clicked Rate Us", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
---
+++ app/src/main/java/com/example/musicplayer/Music.kt
@@ -0,0 +1,39 @@
+package com.example.musicplayer
+
+import android.media.MediaPlayer
+import android.widget.SeekBar
+
+object Music {
+    fun setSeekBar(seekBar: SeekBar, mp: MediaPlayer, volume: Boolean? = null, seekTo: Boolean?=null){
+        seekBar.setOnSeekBarChangeListener(object:SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser){
+                    when{
+                        volume == null -> mp.seekTo(progress)
+                        seekTo == null -> {
+                            val volumeNum = progress/100.0f
+                            mp.setVolume(volumeNum, volumeNum)
+                        }
+                    }
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+
+        })
+    }
+
+    fun createTimeLabel(time:Int):String{
+        var timelabel =""
+        val min = time/1000/60
+        val sec = time/1000%60
+
+        timelabel ="$min"
+        if (sec<10) timelabel+="0"
+        timelabel+=sec
+        return timelabel
+    }
+}
+
